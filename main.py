e519f91a9d2457d8c42fdae9bf25f60f644efab6
sctam <simon@simontam.ca>
2019-12-08 02:25:29 -0700

    Looping feature tested

--- main.py
+++ main.py
@@ -102,41 +102,44 @@ class Window(Frame):
     def _exit_program(self):
         exit()
 
-# def _process_image():
-#     threading.Timer(0.8, _process_image).start()
-#     exec_start = time.time()
+    def _process_image(self, root):
+        print("Test.")
+        # threading.Timer(0.8, _process_image).start()
+        # exec_start = time.time()
 
-#     pil_img = ImageGrab.grab(bbox=(WINDOW_X, WINDOW_Y, WINDOW_X+WINDOW_W, WINDOW_Y+WINDOW_H))  # X1,Y1,X2,Y2
+        # pil_img = ImageGrab.grab(bbox=(WINDOW_X, WINDOW_Y, WINDOW_X+WINDOW_W, WINDOW_Y+WINDOW_H))  # X1,Y1,X2,Y2
 
-#     # Convery PIL to CV
-#     img_rgb = np.array(pil_img) 
-#     img_rgb = img_rgb[:, :, ::-1].copy() # Convert RGB to BGR 
+        # # Convery PIL to CV
+        # img_rgb = np.array(pil_img) 
+        # img_rgb = img_rgb[:, :, ::-1].copy() # Convert RGB to BGR 
 
-#     # Set up base image to look at.
-#     #img_rgb  = cv2.imread('base.jpg')
-#     img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
+        # # Set up base image to look at.
+        # #img_rgb  = cv2.imread('base.jpg')
+        # img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
 
-#     # Match.
-#     res = cv2.matchTemplate(img_gray, TEMPLATE, cv2.TM_CCOEFF_NORMED)
-#     threshold = 0.8
-#     loc = np.where(res >= threshold)
-#     for pt in zip(*loc[::-1]):
-#         cv2.rectangle(img_rgb, pt, (pt[0] + TEMPLATE_W, pt[1] + TEMPLATE_H), (0, 0, 255), 2)
+        # # Match.
+        # res = cv2.matchTemplate(img_gray, TEMPLATE, cv2.TM_CCOEFF_NORMED)
+        # threshold = 0.8
+        # loc = np.where(res >= threshold)
+        # for pt in zip(*loc[::-1]):
+        #     cv2.rectangle(img_rgb, pt, (pt[0] + TEMPLATE_W, pt[1] + TEMPLATE_H), (0, 0, 255), 2)
 
-#     if loc[0].size > 0:
-#         playsound(r'sounds\beep2.wav', False)
-#         print("found")
+        # if loc[0].size > 0:
+        #     playsound(r'sounds\beep2.wav', False)
+        #     print("found")
 
-#     cv2.imwrite('result.png', img_rgb)
+        # cv2.imwrite('result.png', img_rgb)
 
-#     exec_end = (time.time() - exec_start)
-#     print(exec_end)
+        # exec_end = (time.time() - exec_start)
+        # print(exec_end)
+        root.after(2000, self._process_image, root)
 
 def main():
     #_process_image()
 
     root = Tk()
     app = Window(root)
+    root.after(2000, app._process_image, root)
 
     root.mainloop()  
 
